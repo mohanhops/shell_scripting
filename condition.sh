30aefd4c36c84b66564e70685dc9b9a5ac5edd6b
mohanhops <mohanchinna2@gmail.com>
2026-02-09 15:04:27 +0530

    conditions

--- condition.sh
+++ condition.sh
@@ -3,22 +3,13 @@
 #NUMBER=$1
 dnf install mysql -y
 
-output=$?
+output=$? #0 --> success, 1,127 --> failure
 
 
 if [ $output -eq 0 ]; then
    echo "mysql installion is successful"
 else
    echo "mysql installation failed"
-fi
-
-
-if [ $NUMBER -lt 10 ]; then
-    echo "Given number $NUMBER is less than 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "Given number $NUMBER is equal to 10"
-else
-    echo "Given number $NUMBER is greater than to 10"
 fi
 
 # -gt
